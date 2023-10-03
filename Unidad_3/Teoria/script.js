185122bed862662abd2e9ebacdf9499167da4f24
daviddrm52 <davidgranada2015@gmail.com>
2023-10-03 17:16:39 +0200

    tema 3 y cosas del 1 recuperadas

---
+++ Unidad_3/Teoria/script.js
@@ -0,0 +1,18 @@
+console.log("Teoria del tema 3");
+
+/*
+Estructuras de control
+
+Se utilizan para controlar el flujo de un programa
+*/
+
+/*
+if -> Se utiliza para ejecutar un bloque de codigo si se cumple una condicion
+*/
+
+let edad = 18;
+let esMayorDeEdad = (edad >= 18);
+
+if(esMayorDeEdad){
+    console.log("El usuario es mayor de edad");
+};
