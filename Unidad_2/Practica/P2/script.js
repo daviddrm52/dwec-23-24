41abb5a18c635f303ba1f3fcf2df5ea6e2778c1c
daviddrm52 <davidgranada2015@gmail.com>
2023-10-04 19:50:16 +0200

    ejercicio separado para mejor localizacion

--- Unidad_2/Practica/P2/script.js
+++ Unidad_2/Practica/P2/script.js
@@ -208,30 +208,30 @@ console.log(comillas);
  * cierta si y solo si la ciudad es una Metropolis.
  */
 
-function esUnaMetropolis(){
-    //valor que dictamina si es metropolis o no
-    let esMetropolis;    
-    //valores que se tendran en cuenta para ver si es una metropolis o no
-    let esCapital = true;
-    let numeroCiudadanos = 100001;
-    let impuestoPorCiudadano = 200.00;
-    let rentaMinimaCiudad = 720000000;
-    //renta media de la ciudad
-    let rentaMediaCiudad = numeroCiudadanos * (impuestoPorCiudadano * 12);
+// function esUnaMetropolis(){
+//     //valor que dictamina si es metropolis o no
+//     let esMetropolis;    
+//     //valores que se tendran en cuenta para ver si es una metropolis o no
+//     let esCapital = true;
+//     let numeroCiudadanos = 100001;
+//     let impuestoPorCiudadano = 200.00;
+//     let rentaMinimaCiudad = 720000000;
+//     //renta media de la ciudad
+//     let rentaMediaCiudad = numeroCiudadanos * (impuestoPorCiudadano * 12);
 
-    //Visualización de los datos antes del calculo
-    console.log("Es capital: "+esCapital);
-    console.log("Numero de ciudadanos: "+numeroCiudadanos+" censados.");
-    console.log("Impuesto que paga cada ciudadano: "+impuestoPorCiudadano+"€.");
-    console.log("Renta media de la ciudad: "+rentaMediaCiudad+"€.");
+//     //Visualización de los datos antes del calculo
+//     console.log("Es capital: "+esCapital);
+//     console.log("Numero de ciudadanos: "+numeroCiudadanos+" censados.");
+//     console.log("Impuesto que paga cada ciudadano: "+impuestoPorCiudadano+"€.");
+//     console.log("Renta media de la ciudad: "+rentaMediaCiudad+"€.");
 
-    //calculo de si se cumplen los requisitos para metropolis
-    if ((esCapital == true && numeroCiudadanos > 100000) || (numeroCiudadanos > 200000 && rentaMediaCiudad >= rentaMinimaCiudad)){ //en caso de ser capital
-        esMetropolis = true;
-    } else { //si no cumple ninguno de los requisitos
-        esMetropolis = false;
-    }
-    console.log("Es una Metropolis? "+ esMetropolis);
-}
+//     //calculo de si se cumplen los requisitos para metropolis
+//     if ((esCapital == true && numeroCiudadanos > 100000) || (numeroCiudadanos > 200000 && rentaMediaCiudad >= rentaMinimaCiudad)){ //en caso de ser capital
+//         esMetropolis = true;
+//     } else { //si no cumple ninguno de los requisitos
+//         esMetropolis = false;
+//     }
+//     console.log("Es una Metropolis? "+ esMetropolis);
+// }
 
-esUnaMetropolis();
+// esUnaMetropolis();
