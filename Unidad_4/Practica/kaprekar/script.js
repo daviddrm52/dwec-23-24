a606f64e8196f067fb7df57173697a666372eaa4
daviddrm52 <davidgranada2015@gmail.com>
2023-10-30 21:16:38 +0100

    Kaprekar muy duro funcional

---
+++ Unidad_4/Practica/kaprekar/script.js
@@ -0,0 +1,37 @@
+//Numero inicial, con el que jugamos
+let numeroInicial = 8482;
+// Nota mental -> El uso de de menos de 1000, hace que el programa muera.
+// se requiere solución
+//Metodo para poner el numero inicial, dentro del array ascendente y descendente
+let funcion = num => Number(num);
+let numeroAscendente = Array.from(String(numeroInicial), funcion);
+let numeroDescendente = Array.from(String(numeroInicial), funcion);
+let resultado;
+const numeroKaprekar = 6174;
+
+while (resultado !== numeroKaprekar) {
+    //Ordenacion del numero ascendente
+    numeroAscendente.sort().reverse();
+    //Ordenacion del numero descendente
+    numeroDescendente.sort();
+    //Conversión a string para que puedan calcularse los numeros
+    const numeroDescendenteString = numeroAscendente.join("");
+    const numeroAscendenteString = numeroDescendente.join("");
+    console.log(numeroDescendenteString + " " + numeroAscendenteString);
+    let resultado = numeroDescendenteString - numeroAscendenteString;
+    //Conversión a array de nuevo para repetir el proceso hasta encontrar el 6174
+    numeroAscendente = Array.from(String(resultado), funcion);
+    numeroDescendente = Array.from(String(resultado), funcion);
+    console.log(resultado);
+    if(resultado == 6174){
+        break;
+    } else if(resultado == 0){
+        console.log("Numero invalido: "+resultado);
+        break;
+    } else {
+        continue;
+    }
+};
+
+//Habia que empezarlo desde el miercoles, duro
+//Y funciona al menos en menos de 2 horas, durum
